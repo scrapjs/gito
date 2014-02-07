7bd3d18b7f516619d6e6b6e6125e0e3f77b2b0a3
Bent Cardan <bent@nothingsatisfies.com>
2014-02-07 14:32:02 -0500

    added success message

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * Module dependencies.
  */
 
-var type = require('./lib/type')
+var type = require('./lib/type');
 var exec = require('child_process').exec;
-
+var spawn = require('child_process').spawn;
 
 /**
  * Module exports.
@@ -14,29 +14,23 @@ var exec = require('child_process').exec;
 module.exports = function(argvs,cb){
 
   if (type(argvs[2]) == 'string') {
-    
+
     exec('git add -A && git commit --message "'+argvs[2]+'"'
       , function (err,stdout,stderr){
-      
       if(err)throw err;
-
-      //here we need to open buffers for stdout 
-
-      return;
-    
-    });
-    
-    setImmediate(function(){
-
-      exec('git push', function (err,stdout,stderr){
-        
-        if(err)throw err;
-
-        return cb(null,stdout);
+      console.log(stdout);
       
+      var push = spawn('git',['push']);
+      
+      push.on('data',function(data){
+        //console.log(process);
       });
-    
-    });
-  }
+      
+      push.on('close',function(code){
+        console.log('success. pushed up to current remote branch')
+      });
 
+    });
+  
+  }
 }
