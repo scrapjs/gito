f7eb9438c1f61ad07ba3596197841300d2ff88fa
Bent Cardan <bent@nothingsatisfies.com>
2014-02-07 00:53:41 -0500

    test

--- test.js
+++ test.js
@@ -1,21 +1,13 @@
 var assert = require('assert');
-var connect = require('connect');
 var request = require('supertest');
-
-var compress = require('./');
+var mocha = require('mocha')
+var gito = require('./');
 
 describe('gito()', function(){
-
   it('should observe commandline arguments', function(done){
-
     gito(process.argv,function(err,argvs){
-
       assert.equal(process.argv.length, 2);
-
       done();
-
     });
-
   });
-
 });
